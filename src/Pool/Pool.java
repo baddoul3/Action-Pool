b780ed5497357a27d1bf90b140c58434e8541cb8
douae.mahroug <douae.mahroug@etudiant.univ-lille1.fr>
2015-11-05 23:09:48 +0100

    Documentaion de la classe Pool.java

--- src/Pool/Pool.java
+++ src/Pool/Pool.java
@@ -1,28 +1,31 @@
 package Pool;
-import Action.FairScheduler;
+import Action.*;
 /**
  * @author mahroug
- *
+ */
+/**
+ * This class allows to act the swimmers in order to simulate the functioning of a swimming pool
  */
 public class Pool {
-
-	public static void main(String[] args) throws ActionFinishedException {
-		
+	public static void main(String args[]){
+		Basketpool baskets = new Basketpool(6);
 		CubiclePool cubicles = new CubiclePool(3);
-		BasketPool baskets = new BasketPool(6);
 		FairScheduler s = new FairScheduler();
+	
 		s.addAction(new Swimmer("Camille", baskets, cubicles, 6, 4, 8));
 		s.addAction(new Swimmer("Lois", baskets, cubicles, 2, 10, 4));
-		s.addAction(new Swimmer("Louison", baskets, cubicles, 18, 3, 3));
 		s.addAction(new Swimmer("Mae", baskets, cubicles, 10, 18, 10));
 		s.addAction(new Swimmer("Ange", baskets, cubicles, 3, 7, 5));
+		s.addAction(new Swimmer("Louison", baskets, cubicles, 18, 3, 3));
 		s.addAction(new Swimmer("Charlie", baskets, cubicles, 3, 6, 10));
 		s.addAction(new Swimmer("Alexis", baskets, cubicles, 6, 5, 7));
+		
 		int nbSteps = 0;
-		while (!s.isFinished()) {
+		while (!s.isFinished()){
 			nbSteps++;
 			s.doStep();
 		}
-		System.out.println("Finished in " + nbSteps + " steps");
+		System.out.println("Finished in "+nbSteps+" steps");
 	}
 }
+
