05e639f4cc602c1c99b1fd6d678bef99289ede81
EliteBook <EliteBook@EliteBook-PC>
2015-11-05 22:45:03 +0100

    lastest commit

--- src/Pool/Pool.java
+++ src/Pool/Pool.java
@@ -1,7 +1,6 @@
 package Pool;
 import Action.FairScheduler;
 /**
- * 
  * @author mahroug
  *
  */
@@ -9,14 +8,14 @@ public class Pool {
 
 	public static void main(String[] args) throws ActionFinishedException {
 		
-		BasketPool baskets = new BasketPool(6);
 		CubiclePool cubicles = new CubiclePool(3);
+		BasketPool baskets = new BasketPool(6);
 		FairScheduler s = new FairScheduler();
 		s.addAction(new Swimmer("Camille", baskets, cubicles, 6, 4, 8));
 		s.addAction(new Swimmer("Lois", baskets, cubicles, 2, 10, 4));
+		s.addAction(new Swimmer("Louison", baskets, cubicles, 18, 3, 3));
 		s.addAction(new Swimmer("Mae", baskets, cubicles, 10, 18, 10));
 		s.addAction(new Swimmer("Ange", baskets, cubicles, 3, 7, 5));
-		s.addAction(new Swimmer("Louison", baskets, cubicles, 18, 3, 3));
 		s.addAction(new Swimmer("Charlie", baskets, cubicles, 3, 6, 10));
 		s.addAction(new Swimmer("Alexis", baskets, cubicles, 6, 5, 7));
 		int nbSteps = 0;
